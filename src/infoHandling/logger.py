7c91503fd1fedb712fa7ab9c799216c93f9746cf
Shawn <A02248994@aggie.usu.ed>
2023-09-11 18:01:16 -0600

    added folder cration to the logger.py

--- src/infoHandling/logger.py
+++ src/infoHandling/logger.py
@@ -1,10 +1,16 @@
 from datetime import datetime
+import os
 
 '''This class is meant to handel INDIVIDUAL LOGGING for other classes. It has no threading protection. In other words 
 classes should not share access to one of these objects. '''
 class logggerCustom():
     def __init__(self, file):
-        self.__file = open(file, "w+")
+        try :
+            self.__file = open(file, "w+")
+        except :
+            os.mkdir("logs/")
+            self.__file = open(file, "w+")
+
 
     def sendLog(self, text):
         self.__file.write(str(datetime.now()) + ": " + text + "\n")
