40dd3c9848ecafe37c323d2d9599a99e4ca53be3
Shawn <A02248994@aggie.usu.ed>
2023-10-04 09:57:39 -0600

    working on linting

--- src/host/main.py
+++ src/host/main.py
@@ -1,13 +1,20 @@
+'''
+    This module runs everything, its main job is to create and run all of the 
+    system objects and classes. 
+'''
 import time
-import sys
-sys.path.insert(0, "..")
-from  taskHandling.taskHandler import taskHandler
-from  taskHandling.threadWrapper import threadWrapper
-from server import serverHandler
+from taskHandling.taskHandler import taskHandler
+from taskHandling.threadWrapper import threadWrapper
 from database.databaseControl import dataBaseHandler
+from server import serverHandler
+
 
 
 def main():
+    '''
+    This module runs everything, its main job is to create and run all of the 
+    system objects and classes. 
+    '''
     #create a server obj, not it will also create the coms object
     server = serverHandler('144.39.167.206', 5000)
     coms = server.getComs()
@@ -34,8 +41,8 @@ def main():
     threadPool.start() #we need to start all the threads we have collected.
 
     #keep the main thread alive for use to see things running. 
-    running  = True
-    while(running):
+    running = True
+    while running:
         try:
             threadPool.getThreadStatus()
             time.sleep(0.35)
@@ -45,8 +52,6 @@ def main():
     
     threadPool.killTasks()
      
-
-    #Next we wanna start the server and add it as a task we can do. 
 
 if __name__ == "__main__":
     main()
