b40498a516c0d95b6c21c551f87756cb867b2c72
Shawn Jones <sbradenjones16@gmail.com>
2024-02-09 09:15:50 -0800

    working on liniting

--- src/host/server.py
+++ src/host/server.py
@@ -4,8 +4,6 @@
 #python imports
 import logging
 from flask import Flask, render_template, request , send_from_directory, jsonify
-from threading import Lock
-import random
 from datetime import datetime
 import os
 
@@ -19,7 +17,11 @@ from DTOs.logger_dto import logger_dto
 from DTOs.print_message_dto import print_message_dto
 
 class serverHandler(threadWrapper):
+    '''
+        This class is the server for the whole system. It hands serving the webpage and routing request to there respective classes. 
+    '''
     def __init__(self, hostName, serverPort, coms, cmd, messageHandler:serverMessageHandler, messageHandlerName:str, serial_writter_name:str, serial_listener_name:str):
+        # pylint: disable=w0612
         self.__function_dict = { #NOTE: I am only passing the function that the rest of the system needs 
             'run' : self.run,
             'kill_Task' : self.kill_Task,
@@ -57,6 +59,9 @@ class serverHandler(threadWrapper):
         self.__favicon_directory = os.path.join(self.app.root_path, 'static')
         self.setup_routes()     
     def setup_routes(self):
+        '''
+            This function sets up all the git reqest that can be accessed by the webpage.
+        '''
         #Paths that the server will need 
         self.app.route('/')(self.status_report)
         self.app.route('/open_data_stream')(self.open_data_stream)
@@ -77,8 +82,14 @@ class serverHandler(threadWrapper):
         #the paths caught by this will connect to the users commands they add
         self.app.add_url_rule('/<path:unknown_path>', 'handle_unknown_path',  self.handle_unknown_path)
     def facicon(self):
-         return send_from_directory(self.__favicon_directory, 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+        '''
+            Returns image in the cornor of the tab. 
+        '''
+        return send_from_directory(self.__favicon_directory, 'favicon.ico', mimetype='image/vnd.microsoft.icon')
     def handle_unknown_path(self, unknown_path):
+        '''
+            This handles path that are not automatically added. (Basically user defined commands.)
+        '''
         path = unknown_path.split("/")
         self.__log.send_log("Message recived: " + str(path))
         #this  if statement decides if it is going to run a prebuilt command or run one of the 
@@ -90,8 +101,14 @@ class serverHandler(threadWrapper):
         self.__coms.print_message(dto2, 2)
         return message
     def serve_page_manigure(self):
+        '''
+            Returns java script to the browser. So that it can be run in browser. 
+        '''
         return send_from_directory('source', 'page_manigure.js')
     def status_report(self):
+        '''
+            collects and then send the logs from the commupter. 
+        '''
         dto = print_message_dto('Got status_report request')
         self.__coms.print_message(dto, 3)
         
@@ -106,13 +123,17 @@ class serverHandler(threadWrapper):
         #get byte report
         return render_template('status_report.html', data=data, prem_data=prem_data, thread_report=thread_report, status=status)
     def get_prem_message_log(self):
+        '''
+            Gets the logs witch will presist accross run time. 
+        '''
         #make a request for the messages
-        id = self.__coms.send_request(self.__message_handler_name, ['get_prem_message_log']) #send the server the info to display
+        id_request = self.__coms.send_request(self.__message_handler_name, ['get_prem_message_log']) #send the server the info to display
         data_obj = None
         #wait for the messages to be returneds
         while data_obj is None:
-            data_obj = self.__coms.get_return(self.__message_handler_name, id)
-        if len(data_obj) > 0:data = self.make_web_dto(data_obj[0])
+            data_obj = self.__coms.get_return(self.__message_handler_name, id_request)
+        if len(data_obj) > 0: 
+            data = self.make_web_dto(data_obj[0])
         else : 
             data = [{
             'time' : 'Not avalible',
@@ -120,6 +141,9 @@ class serverHandler(threadWrapper):
             }]        
         return data
     def make_web_dto(self, data_in):
+        '''
+            Makes a dict that the webpage and understand. (For logging messages.)
+        '''
         data = []
         for i in data_in:
             data.append({
@@ -128,13 +152,17 @@ class serverHandler(threadWrapper):
             })
         return data
     def get_updated_prem_logger_report(self):
+        '''
+            This function fetches the prem log report.
+        '''
         #make a request for the messages
-        id = self.__coms.send_request(self.__message_handler_name, ['get_prem_message_log']) #send the server the info to display
+        id_request = self.__coms.send_request(self.__message_handler_name, ['get_prem_message_log']) #send the server the info to display
         data_obj = None
         #wait for the messages to be returneds
         while data_obj is None:
-            data_obj = self.__coms.get_return(self.__message_handler_name, id)
-        if len(data_obj) > 0:data = self.make_web_dto(data_obj[0])
+            data_obj = self.__coms.get_return(self.__message_handler_name, id_request)
+        if len(data_obj) > 0:
+            data = self.make_web_dto(data_obj[0])
         else : 
             data = [{
             'time' : 'Not avalible',
@@ -142,13 +170,17 @@ class serverHandler(threadWrapper):
             }]
         return jsonify(prem_logger_report=data)
     def get_loggs_data(self):
+        '''
+            Gets the logs update, for the system. 
+        '''
         #make a request for the messages
-        id = self.__coms.send_request(self.__message_handler_name, ['get_messages']) #send the server the info to display
+        id_request = self.__coms.send_request(self.__message_handler_name, ['get_messages']) #send the server the info to display
         data_obj = None
         #wait for the messages to be returneds
         while data_obj is None:
-            data_obj = self.__coms.get_return(self.__message_handler_name, id)
-        if len(data_obj) > 0:data = self.make_web_dto(data_obj[0])
+            data_obj = self.__coms.get_return(self.__message_handler_name, id_request)
+        if len(data_obj) > 0:
+            data = self.make_web_dto(data_obj[0])
         else : 
             data = [{
             'time' : 'Not avalible',
@@ -156,13 +188,17 @@ class serverHandler(threadWrapper):
             }]        
         return data
     def get_updated_logger_report(self):
+        '''
+            This is the function that the webpage calls to get the logger update. 
+        '''
         #make a request for the messages
-        id = self.__coms.send_request(self.__message_handler_name, ['get_messages']) #send the server the info to display
+        id_request = self.__coms.send_request(self.__message_handler_name, ['get_messages']) #send the server the info to display
         data_obj = None
         #wait for the messages to be returneds
         while data_obj is None:
-            data_obj = self.__coms.get_return(self.__message_handler_name, id)
-        if len(data_obj) > 0:data = self.make_web_dto(data_obj[0])
+            data_obj = self.__coms.get_return(self.__message_handler_name, id_request)
+        if len(data_obj) > 0:
+            data = self.make_web_dto(data_obj[0])
         else : 
             data = [{
             'time' : 'Not avalible',
@@ -170,12 +206,15 @@ class serverHandler(threadWrapper):
             }]
         return jsonify(logger_report=data)      
     def get_thread_report(self):
+        '''
+            Get the threading report from the system.
+        '''
         #make a request for the messages
-        id = self.__coms.send_request(self.__message_handler_name, ['get_thread_report']) #send the server the info to display
+        id_request = self.__coms.send_request(self.__message_handler_name, ['get_thread_report']) #send the server the info to display
         data_obj = None
         #wait for the messages to be returneds
         while data_obj is None:
-            data_obj = self.__coms.get_return(self.__message_handler_name, id)
+            data_obj = self.__coms.get_return(self.__message_handler_name, id_request)
         data = []
         if len(data_obj[0]) > 0:
             for report in data_obj[0]:
@@ -196,16 +235,22 @@ class serverHandler(threadWrapper):
             )
         return data
     def get_updated_thread_report(self):
+        '''
+            Server calls this to get the thread report.
+        '''
         data = self.get_thread_report()
         return jsonify(thread_report = data)
     def get_status_report(self):
+        '''
+            Web page class this function to get the status update
+        '''
         #make a request for the messages
-        id = self.__coms.send_request(self.__message_handler_name, ['get_report_status']) #send the server the info to display
+        id_request = self.__coms.send_request(self.__message_handler_name, ['get_report_status']) #send the server the info to display
         data_obj = None
 
         #wait for the messages to be returneds
         while data_obj is None:
-            data_obj = self.__coms.get_return(self.__message_handler_name, id)
+            data_obj = self.__coms.get_return(self.__message_handler_name, id_request)
         data = []
         if len(data_obj[0]) < 1: 
             data = [{
@@ -221,25 +266,37 @@ class serverHandler(threadWrapper):
                     })
         return data
     def get_update_status_report(self):
+        '''
+            gets the status report from the system. 
+        '''
         data = self.get_status_report()
         return jsonify(status_list = data)
     def get_serial_info_update(self):
+        '''
+            Gets the serial feed infromation from the system. 
+        '''
         #make a request for the messages
-        id = self.__coms.send_request(self.__message_handler_name, ['get_byte_report']) #send the server the info to display
+        id_request = self.__coms.send_request(self.__message_handler_name, ['get_byte_report']) #send the server the info to display
         data_obj = None
         #wait for the messages to be returneds
         while data_obj is None:
-            data_obj = self.__coms.get_return(self.__message_handler_name, id)
+            data_obj = self.__coms.get_return(self.__message_handler_name, id_request)
         return jsonify(data_obj)
     def serial_run_request(self):
+        '''
+            Send a request to the serial writter to exicute a command.
+        '''
         #make a request for the messages
-        id = self.__coms.send_request(self.__serial_writter_name, ['write_to_serial_port', request.args.get('serial_command')]) #send the request to the serial writter
+        id_request = self.__coms.send_request(self.__serial_writter_name, ['write_to_serial_port', request.args.get('serial_command')]) #send the request to the serial writter
         data_obj = None
         #wait for the messages to be returneds
         while data_obj is None:
-            data_obj = self.__coms.get_return(self.__serial_writter_name, id)
+            data_obj = self.__coms.get_return(self.__serial_writter_name, id_request)
         return data_obj
     def start_serial(self):
+        '''
+            This starts a reconfigured serial listener and writter. 
+        '''
         boud_rate = request.args.get('boud_rate')
         stop_bit = request.args.get('stop_bit')
 
@@ -257,43 +314,75 @@ class serverHandler(threadWrapper):
         data_obj = data_obj_listerner + data_obj_writter
         return data_obj
     def serial_running_writter(self):
+        '''
+            Checks if the serial writter is running and returns that to the server. 
+        '''
         id_writter = self.__coms.send_request(self.__serial_writter_name, ['get_connected']) #send the request to the serial writter
         data_obj_writter = None
         #wait for the messages to be returneds
         while data_obj_writter is None:
             data_obj_writter = self.__coms.get_return(self.__serial_writter_name, id_writter)
-        if type(data_obj_writter) is bool: data_obj = "Online" if data_obj_writter else "Not Online"
+        if isinstance(data_obj_writter, bool): 
+            data_obj = "Online" if data_obj_writter else "Not Online"
         else : data_obj = "Not Online"
         return data_obj
     def serial_running_listener(self):
+        '''
+            Checks fi the serial reader is running and returns that to the server.
+        '''
         id_listener = self.__coms.send_request(self.__serial_listener_name, ['get_connected']) #send the request to the serial writter
         data_obj_listen = None
         #wait for the messages to be returneds
         while data_obj_listen is None:
             data_obj_listen = self.__coms.get_return(self.__serial_listener_name, id_listener)
-        if type(data_obj_listen) is bool: data_obj = "Online" if data_obj_listen else "Not Online"
+        if isinstance(data_obj_listen, bool):
+            data_obj = "Online" if data_obj_listen else "Not Online"
         else : data_obj = "Not Online"
         return data_obj
     def run(self):
+        '''
+            This is the run function for the server. 
+        '''
         self.__log.send_log("Test Server started http://%s:%s" % (self.__hostName, self.__serverPort))
         dto = logger_dto(message="Server started http://%s:%s" % (self.__hostName, self.__serverPort), time=str(datetime.now()))
         self.__coms.send_message_prement(dto, 2)
         super().set_status("Running")
         self.app.run(debug=False, host=self.__hostName, port=self.__serverPort)
     def kill_Task(self):
+        '''
+            This closes the Server, after the kill command is recived.
+        '''
         super().kill_Task()
         self.__log.send_log("Server stopped.")
         self.__log.send_log("Quite command recived.")
-    def getComs(self):
-        return self.__coms
     def setHandlers(self, db):
+        '''
+            Sets up the data base configuration. 
+        '''
         self.__cmd.collect_commands(db)    
     def open_data_stream(self):
+        '''
+            Return the html page for the data stream page.
+        '''
         return render_template('data_stream.html')
     def open_sensor(self):
+        '''
+            Returns html for the senseor page
+        '''
         return render_template('sensor.html')
     def command(self):
+        '''
+            Returns html for the command page
+        '''
         table_data = self.__cmd.get_commands_webpage()
         return render_template('Command.html', table_data=table_data)
     def get_message_handler(self):
+        '''
+            Return the servers message handler object. 
+        '''
         return self.__message_handler
+    def getComs(self):
+        '''
+            Get the coms object, the coms object is how ever thread talks to each other. 
+        '''
+        return self.__coms
