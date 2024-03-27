71905261714fb1348e70418eff20eb20bc770af3
Shawn Jones <sbradenjones16@gmail.com>
2024-03-27 23:37:52 +0000

    making the system costants public so every one can use and see them

---
+++ src/host/system_constants.py
@@ -0,0 +1,7 @@
+#This files holds the definition of where to find different tools that the system has access to. 
+interface_writer_list = {}
+interface_listener_list = {}
+server = None
+sensors_config = {}
+database_name = ''
+board_serial_listener_name = ''
