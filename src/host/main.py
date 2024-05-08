c5bab856e8b96378c3f25746cad9474f7372a2bd
Shawn Jones <sbradenjones16@gmail.com>
2024-05-08 21:03:27 +0100

    update to work on distrubted system

--- src/host/main.py
+++ src/host/main.py
@@ -4,31 +4,14 @@
 '''
 #python built in imports
 import time
-import datetime
 import yaml
 
 #python custom imports
-import system_constants as sensor_config # pylint: disable=e0401 
-from threading_python_api.taskHandler import taskHandler # pylint: disable=e0401 
-from database_python_api.database_control import DataBaseHandler # pylint: disable=e0401
+from threading_python_api.taskHandler import taskHandler # pylint: disable=e0401
+from python_serial_api.serial_listener import serial_listener # pylint: disable=e0401
+from python_serial_api.serial_writer import serial_writer # pylint: disable=e0401
 from logging_system_display_python_api.messageHandler import messageHandler # pylint: disable=e0401
-from cmd_inter import cmd_inter # pylint: disable=e0401
-from server import serverHandler # pylint: disable=e0401
-from server_message_handler import serverMessageHandler # pylint: disable=e0401
-from pytesting_api.test_runner import test_runner # pylint: disable=e0401
-from pytesting_api import global_test_variables # pylint: disable=e0401
-
-#These are some Debugging tools I add, Turning off the display is really useful for seeing errors, because the terminal wont get erased every few milliseconds with the display on.
-NO_SERIAL_LISTENER = False
-NO_SERIAL_WRITER = False
-NO_SENSORS = False
-
-if not NO_SERIAL_LISTENER:
-    from python_serial_api.serial_listener import serial_listener # pylint: disable=e0401
-if not NO_SERIAL_WRITER:
-    from python_serial_api.serial_writer import serial_writer # pylint: disable=e0401
-if not NO_SENSORS:
-    from sensor_interface_api.collect_sensor import sensor_importer # pylint: disable=e0401
+from data_publisher import data_publisher # pylint: disable=e0401
 
 def main():
     '''
@@ -40,11 +23,6 @@ def main():
     # Load the YAML file
     with open("main.yaml", "r") as file:
         config_data = yaml.safe_load(file)
-    
-    test_interval = config_data.get("test_interval", 0)
-    failed_test_path = config_data.get("failed_test_path", 0)
-    passed_test_path = config_data.get("passed_test_path", 0)
-    max_passed_test = config_data.get("max_passed_test", 0)
 
     batch_size_1 = config_data.get("batch_size_1", 0)
     batch_size_2 = config_data.get("batch_size_2", 0)
@@ -57,55 +35,21 @@ def main():
     uart_0 = config_data.get("uart_0", "")
     uart_2 = config_data.get("uart_2", "")
 
-    serial_listener_list = config_data.get("serial_listener_list", [])
-    serial_writer_list = config_data.get("serial_writer_list", [])
-
     hostname = config_data.get("hostname", "")
-    port = config_data.get("port", 0)
-    server_listener_name = config_data.get("server_listener_name", "")
-    server_name_host = config_data.get("server_name_host", "")
 
-    data_base = config_data.get("data_base", "")
-    if not NO_SENSORS:
-        # Sensor configs
-        sensor_config_dict = config_data.get("sensor_config_dict", {})
+    port_serial_1 = config_data.get("port_serial_1", "")
+    port_serial_2 = config_data.get("port_serial_2", "")
 
-        # set up the config file
-        sensor_config.interface_listener_list = serial_listener_list
-        sensor_config.interface_writer_list = serial_writer_list
-        sensor_config.server = server_listener_name
-        sensor_config.sensors_config = sensor_config_dict
-        sensor_config.database_name = data_base
 
     ########################################################################################
 
-    ########### Set up server, database, and threading interface ########### 
-    #create a server obj, not it will also create the coms object #144.39.167.206
-    coms = messageHandler(server_name=server_listener_name, hostname=hostname)
-    #make database object 
-    dataBase = DataBaseHandler(coms, is_gui=False)
-    #now that we have the data base we can collect all of our command handlers.s
-    cmd = cmd_inter(coms, dataBase)
-    #now that we have all the commands we can make the server
-    #note because the server requires a thread to run, it cant have a dedicated thread to listen to coms like
-    #other classes so we need another class object to listen to internal coms for the server.
-    server_message_handler = serverMessageHandler(coms=coms)
-    server = serverHandler(hostname, port, coms, cmd, serverMessageHandler, server_listener_name, serial_writer_name=serial_writer_list, serial_listener_name=serial_listener_list, failed_test_path=failed_test_path, passed_test_path=passed_test_path)
-    
-
+    ########### Set up threading interface ########### 
+    coms = messageHandler(logging=False, hostname=hostname) # for the pi to decrease load, we are turning off logging. 
     #first start our thread handler and the message handler (coms) so we can start reporting
     threadPool = taskHandler(coms) # NOTE: we dont need to add a coms task because it add automatically.
 
     #Make sure to add the  thread handler to coms so that we can send threading requests
     coms.set_thread_handler(threadPool)
-
-    #Next we want to start the server and give it its own thread
-    threadPool.add_thread(server.run, server_name_host, server) #this thread services incoming web request
-    threadPool.add_thread(server_message_handler.run, server_listener_name, server_message_handler) #this thread services incoming request from other threads on the system. 
-    #Now start the data base and give it a thread
-    threadPool.add_thread(dataBase.run, data_base, dataBase)
-    # add dummy data generator, for now this only works for the screen
-    # threadPool.add_thread(dummy.test2, 'Dummy data collector', dummy)
 
     threadPool.start() #we need to start all the threads we have collected.
     ########################################################################################
@@ -113,126 +57,45 @@ def main():
 
     ########### Set up seral interface ###########  
     # create the ser_listener
-    if not NO_SERIAL_LISTENER:
-        # Serial listener one
-        ser_listener = serial_listener(coms = coms, batch_size=batch_size_1, thread_name=serial_listener_name, stopbits=1, pins=uart_0)
-        threadPool.add_thread(ser_listener.run, serial_listener_name, ser_listener)
+    # Serial listener one
+    ser_listener = serial_listener(coms = coms, batch_size=batch_size_1, thread_name=serial_listener_name, stopbits=1, pins=uart_0)
+    threadPool.add_thread(ser_listener.run, serial_listener_name, ser_listener)
 
-        # Serial listener two
-        ser_2_listener = serial_listener(coms = coms, batch_size=batch_size_2, thread_name=serial_listener_2_name, baudrate=9600, stopbits=1, pins=uart_2)
-        threadPool.add_thread(ser_2_listener.run, serial_listener_2_name, ser_2_listener)
-        
-        threadPool.start() #start the new task
+    # Serial listener two
+    ser_2_listener = serial_listener(coms = coms, batch_size=batch_size_2, thread_name=serial_listener_2_name, baudrate=9600, stopbits=1, pins=uart_2)
+    threadPool.add_thread(ser_2_listener.run, serial_listener_2_name, ser_2_listener)
+    
+    threadPool.start() #start the new task
 
     # create the ser_writer
-    if not NO_SERIAL_WRITER:
-        # Serial writer one
-        ser_writer = serial_writer(coms = coms, thread_name=serial_writer_name, pins=uart_0)
-        threadPool.add_thread(ser_writer.run, serial_writer_name, ser_writer)
+    # Serial writer one
+    ser_writer = serial_writer(coms = coms, thread_name=serial_writer_name, pins=uart_0)
+    threadPool.add_thread(ser_writer.run, serial_writer_name, ser_writer)
 
-        # Serial writer two
-        ser_2_writer = serial_writer(coms = coms, thread_name=serial_writer_2_name, baudrate=9600, stopbits=1, pins=uart_2)
-        threadPool.add_thread(ser_2_writer.run, serial_writer_2_name, ser_2_writer)
-        
-        threadPool.start() #start the new task
-    ########################################################################################
+    # Serial writer two
+    ser_2_writer = serial_writer(coms = coms, thread_name=serial_writer_2_name, baudrate=9600, stopbits=1, pins=uart_2)
+    threadPool.add_thread(ser_2_writer.run, serial_writer_2_name, ser_2_writer)
     
-    ########### Set up sensor interface ###########
-    if not NO_SENSORS:
-        # create the sensors interface
-        importer = sensor_importer() # create the importer object
-        importer.import_modules() # collect the models to import
-        importer.instantiate_sensor_objects(coms=coms) # create the sensors objects.
-
-        sensors = importer.get_sensors() #get the sensors objects
-        
-        # create a thread for each sensor and then start them. 
-        for sensor in sensors:
-            threadPool.add_thread(sensor.run, sensor.get_sensor_name(), sensor)
-        threadPool.start()
-
-        # give the webpage gain access to the sensors.
-        server.set_sensor_list(sensors=sensors)
-
+    threadPool.start() #start the new task
     ########################################################################################
 
-    ######################### Set up unit tests ############################################
-    global_test_variables.coms = coms
-    global_test_variables.db_name = data_base
-    global_test_variables.session_id = f"Start_up {datetime.datetime.now()}"
+    ########### Set up publishers for our listeners ###########
+    data_publisher_one = data_publisher(coms=coms, live_feed=serial_listener_name)
+    data_publisher_two = data_publisher(coms=coms, live_feed=serial_listener_2_name)
 
-    # Now lets set up the session table in the database, 
-    table_name = 'session_record'
-    table_structure = {
-        table_name : [['session_id', 0, 'string'], ['start_time', 0, 'string'], ['end_time', 0, 'string'], ['description', 0, 'string']],
-    }
-    # This is the main file, so we do have access to the data base object so we could call the create_table_external function directly, 
-    # however there are two major problems, 
-    # 1: we would actually need to call 'make_request' to get the database to actually run our request.
-    # 2: this provides a good example for the user of how to use the coms obj.
-    coms.send_request(data_base, ['create_table_external', table_structure]) 
-
-    #Now create the test_runner object
-    test_interface = test_runner(failed_test_path=failed_test_path, passed_test_path=passed_test_path, max_files_passed=max_passed_test)
+    print(data_publisher_one.start_data_publisher([port_serial_1, 'live']))
+    print(data_publisher_two.start_data_publisher([port_serial_2, 'live']))
     ########################################################################################
 
-
-
-    #Good line if you need to test a thread crashing. 
-    # coms.send_request('Data Base', ['save_byte_data', 'NO_TABLE', 0, 'serial listener'])
-
-    #keep the main thread alive for use to see things running. 
-    running = True
-    session_start_time = datetime.datetime.now()
-    session_end_time = datetime.datetime.now()
-    session_was_running = False
-    session = f'start up session {session_start_time}'
-    description = ''
-    session_running = False
-    test_start_time = datetime.datetime.now()
 
     #Run the main loop handle session, testing, and threading overhead
+    running = True
+
     while running:
         try:
             threadPool.get_thread_status() #this commands the threads to report to the server how they are running. 
-            
-            session, description, session_running = server.get_session_info() #get the current session settings. 
-
-            if session_running: 
-                #tell users we have started
-                if not session_was_running:
-                    coms.report_additional_status('Main', f'Main thread : Running Session {datetime.datetime.now()}')
-
-                session_was_running = True
-
-                #run test if it is time
-                if datetime.datetime.now() - test_start_time >= test_interval:
-                    coms.report_additional_status('Main', f'Main thread : Running  test {datetime.datetime.now()}')
-                    test_interface.run_tests()
-                    coms.report_additional_status('Main', f'Main thread : Completed test {datetime.datetime.now()}')
-            elif session_was_running:
-                #report the session information
-                data = {
-                    'session_id' : [session],
-                    'start_time' : [str(session_start_time)],
-                    'end_time' : [str(session_end_time)],
-                    'description' : [description],
-                }
-                coms.send_request(data_base, ['save_data_group', table_name, data, 'main'])
-                session_was_running = False
-            else :
-                session_start_time = datetime.datetime.now()
-                session_was_running = False
-
-
+            time.sleep(1) # sleep for one second.
         except KeyboardInterrupt:
-            data = {
-                    'session_id' : [session],
-                    'start_time' : [str(session_start_time)],
-                    'end_time' : [str(session_end_time)],
-                    'description' : [description],
-                }
-            coms.send_request(data_base, ['save_data_group', table_name, data, 'main'])
             running = False
             print('\n>>> Main thread Shutdown Commanded, please wait.')        
     
